9942910d08993c7c308e2b247ff9e9bada39c0f4
Dan Moore <dan@fusionauth.io>
2021-04-16 15:01:59 -0600

    Revert "Remove entry point, not needed"
    This reverts commit 6043cde74cf8959e251302ae8d896782aefab6e4.

---
+++ src/Security/Firewall/EntryPoint.php
@@ -0,0 +1,21 @@
+<?php
+namespace App\Security\Firewall;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Security\Core\Exception\AuthenticationException;
+use Symfony\Component\Security\Http\EntryPoint\AuthenticationEntryPointInterface;
+
+class EntryPoint implements AuthenticationEntryPointInterface{
+  private $url;
+  public function __construct($url){
+    $this->url = $url;
+  }
+  public function start(Request $request, AuthenticationException $authException = null){
+      $response = new Response(
+        '',
+        Response::HTTP_FOUND, //for 302 and Response::HTTP_TEMPORARY_REDIRECT for HTTP307 read about in [Response][4]
+        array('Location'=>$this->url)); 
+      return $response;
+  }
+}
