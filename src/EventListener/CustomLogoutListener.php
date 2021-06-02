f5b5594d0d01c86c369eba52f1785acbb683fe08
Dan Moore <dan@fusionauth.io>
2021-06-02 16:09:59 -0600

    Update logout to work multi tenant.

--- src/EventListener/CustomLogoutListener.php
+++ src/EventListener/CustomLogoutListener.php
@@ -2,27 +2,59 @@
 
 namespace App\EventListener;
 
+use App\Entity\Tenant; 
+use Doctrine\ORM\EntityManagerInterface;
+use Psr\Log\LoggerInterface;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\Security\Http\Event\LogoutEvent;
 
 class CustomLogoutListener
 {
-    private $fusionauthClientId;
+    private $controlPlaneClientId;
+    private $controlPlaneHostname;
     private $fusionauthBase;
+    private $logger;
+    private $entityManager;
 
-    public function __construct($fusionauthBase, $fusionauthClientId)
+    public function __construct(LoggerInterface $logger, EntityManagerInterface $entityManager, String $fusionauthBase, String $controlPlaneClientId, String $controlPlaneHostname)
     {
+        $this->logger = $logger;
+        $this->entityManager = $entityManager;
         $this->fusionauthBase = $fusionauthBase;
-        $this->fusionauthClientId = $fusionauthClientId;
+        $this->controlPlaneClientId = $controlPlaneClientId;
+        $this->controlPlaneHostname = $controlPlaneHostname;
     }
 
    public function onSymfonyComponentSecurityHttpEventLogoutEvent(LogoutEvent $event)
     {
-//TBD need to handle different hostnames
+        $host = $event->getRequest()->getHost();
+
+        // convert ppvcfoo.fusionauth.io to ppvcfoo so we can look up the tenant
+        $hostname = str_replace('.fusionauth.io','',$host); // TBD have 'fusionauth.io' be a parameter
+        $clientId = $this->retrieveClientId($hostname, $this->entityManager);
 
         $fusionauth_base = $this->fusionauthBase;
-        $client_id = $this->fusionauthClientId;
-        $response = new RedirectResponse($fusionauth_base.'/oauth2/logout?client_id='.$client_id);
+
+        $response = new RedirectResponse($fusionauth_base.'/oauth2/logout?client_id='.$clientId);
         $event->setResponse($response);
     }
+
+  // TBD somewhat duplicated code with the loginurl controller
+  private function retrieveClientId($hostname, $entityManager): String
+  {
+    $client_id = '';
+
+    if ($hostname === $this->controlPlaneHostname) {
+      $client_id = $this->controlPlaneClientId;
+    } else { 
+      $repository = $entityManager->getRepository(Tenant::class);
+      $tenant = $repository->findOneBy(array('hostname'=>$hostname));
+      if ($tenant) {
+        $client_id = $tenant->getApplicationId();
+      } else {
+        // TBD what if someone is probing our allowed hostnames
+      }
+    }
+    return $client_id;
+  }
 }
